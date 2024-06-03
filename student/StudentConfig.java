123fcd6668143f6371dc97b8cefcc83364264789
kbhargav98 <kickbuttowsiki@gmail.com>
2024-06-02 20:43:40 -0500

    Initial commit - Spring Boot Application

---
+++ student/StudentConfig.java
@@ -0,0 +1,34 @@
+package com.bhargavcode.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.AUGUST;
+import static java.time.Month.OCTOBER;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+
+                   Student bhargav= new Student(
+                            "bhargav",
+                            "bhargav@gmail.com",
+
+                            LocalDate.of(2001, AUGUST,9));
+          Student Navya=  new Student(
+                    "Navya",
+                    "Navya@gmail.com",
+
+                    LocalDate.of(2000, OCTOBER,9));
+            repository.saveAll(List.of(saketh,Navya));
+
+        };
+    };
+}
